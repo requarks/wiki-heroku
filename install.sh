76b632e37494738d91133fea950ef6afe53055c9
Nicolas Giard <github@ngpixel.com>
2019-02-08 18:17:12 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -7,9 +7,9 @@ echo "============================="
 VERSION=$(curl -L -s -S https://beta.requarks.io/api/version/stable)
 
 echo "-> Fetching latest build..."
-curl -L -s -S https://github.com/Requarks/wiki/releases/download/v$VERSION/wiki-js.tar.gz | tar xz -C ./wiki
+curl -L -s -S https://github.com/Requarks/wiki-v1/releases/download/v$VERSION/wiki-js.tar.gz | tar xz -C ./wiki
 
 echo "-> Fetching dependencies..."
-curl -L -s -S https://github.com/Requarks/wiki/releases/download/v$VERSION/node_modules.tar.gz | tar xz -C ./wiki
+curl -L -s -S https://github.com/Requarks/wiki-v1/releases/download/v$VERSION/node_modules.tar.gz | tar xz -C ./wiki
 
 echo "-> Installation Complete"
