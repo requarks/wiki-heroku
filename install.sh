c55519bbdb0c3423d4f82065515ffd306614e127
carloshfmaciel <carloshfmaciel@gmail.com>
2019-03-21 01:46:21 -0300

    Update install.sh
    setting last stable version and fix download url

--- install.sh
+++ install.sh
@@ -4,12 +4,12 @@ echo "============================="
 echo "WIKI.JS - HEROKU INSTALLATION"
 echo "============================="
 
-VERSION=$(curl -L -s -S https://beta.requarks.io/api/version/stable)
+VERSION=1.0.102
 
 echo "-> Fetching latest build..."
-curl -L -s -S https://github.com/Requarks/wiki-v1/releases/download/v$VERSION/wiki-js.tar.gz | tar xz -C ./wiki
+curl -L -s -S https://github.com/Requarks/wiki/releases/download/v$VERSION/wiki-js.tar.gz | tar xz -C ./wiki
 
 echo "-> Fetching dependencies..."
-curl -L -s -S https://github.com/Requarks/wiki-v1/releases/download/v$VERSION/node_modules.tar.gz | tar xz -C ./wiki
+curl -L -s -S https://github.com/Requarks/wiki/releases/download/v$VERSION/node_modules.tar.gz | tar xz -C ./wiki
 
 echo "-> Installation Complete"
