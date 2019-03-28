acfc84891c4bda396d795687f9d3d59af8ccfda9
Nicolas Giard <github@ngpixel.com>
2019-03-27 20:46:51 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@ echo "============================="
 echo "WIKI.JS - HEROKU INSTALLATION"
 echo "============================="
 
-VERSION=$(curl -L -s -S https://beta.requarks.io/api/version/stable)
+VERSION=$(curl -L -s -S https://raw.githubusercontent.com/Requarks/wiki-v1/master/CURRENT)
 
 echo "-> Fetching latest build..."
 curl -L -s -S https://github.com/Requarks/wiki-v1/releases/download/v$VERSION/wiki-js.tar.gz | tar xz -C ./wiki
